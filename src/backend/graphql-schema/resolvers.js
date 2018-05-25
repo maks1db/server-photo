4f5c2d13d6d57033ddfac4bf0ede02088954584c
Maksim Skvortsov <maksskvor@yandex.ru>
2018-05-25 23:21:30 +0300

    add resolvers copy and rename

--- src/backend/graphql-schema/resolvers.js
+++ src/backend/graphql-schema/resolvers.js
@@ -6,6 +6,7 @@ const getStats = require('../../helpers/models/getStats');
 const pathItem = require('../../helpers/models/pathItem');
 const itFolder = require('../../helpers/models/itFolder');
 const toMB = require('../../helpers/models/toMB');
+const copyItem = require('../../helpers/models/copyItem');
 
 const resolvers = {
     Query: {
@@ -57,19 +58,60 @@ const resolvers = {
             return {
                 name,
                 path: result,
-                itFolder: itFolder(result),
+                itFolder: true,
                 dateCreate: stats.birthtime,
                 size: toMB(stats.size)
             };
         },
         renameItem: async (__, arg) => {
             const { file, name } = arg;
-            const itFolder = itFolder(file);
 
             const basename = path.basename(file);
-            const type = itFolder ? '' : basename.split('.')[1];
+            const type = path.extname(file);
+            const folder = file.replace(basename, '');
+
+            const newFile = path.resolve(folder, name + type);
+            if (fs.existsSync(newFile)) {
+                return {};
+            }
+
+            fs.renameSync(file, newFile);
+            const stats = await getStats(newFile);
+            return {
+                name: name + type,
+                path: newFile,
+                itFolder: false,
+                dateCreate: stats.birthtime,
+                size: toMB(stats.size)
+            };
         },
-        copyItem: async (__, arg) => {}
+        copyItems: async (__, arg) => {
+            const { files, folder } = arg;
+
+            return files.map(async file => {
+                const basename = path.basename(file);
+                const type = path.extname(file);
+
+                const exists = fs.existsSync(path.resolve(folder, basename));
+                const newFile = exists
+                    ? basename.replace(type, '') +
+                      `_${new Date().valueOf()}${type}`
+                    : basename;
+
+                copyItem(file, path.resolve(folder, newFile));
+
+                const result = path.resolve(folder, newFile);
+                const stats = await getStats(result);
+
+                return {
+                    name: path.basename(result),
+                    path: result,
+                    itFolder: itFolder(result),
+                    dateCreate: stats.birthtime,
+                    size: toMB(stats.size)
+                };
+            });
+        }
     }
 };
 
