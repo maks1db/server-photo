5e7e7817a04da5e1ba7a86e834bc78ee104746a3
maxim.skvortsov <maksskvor@yandex.ru>
2019-07-19 12:41:11 +0300

    Fix loading img load (add observer)

--- src/frontend/components/Render/Item.jsx
+++ src/frontend/components/Render/Item.jsx
@@ -9,6 +9,13 @@ const img = {
     file: '/assets/images/file.png'
 };
 
+const LOADING_TIMEOUT = 2000;
+
+const defaultOptions = {
+    rootMargin: '100px',
+    threshold: 0.25
+};
+
 export const itemSrc = (path, src = 'small') =>
     `${
         process.env.DEV ? 'http://localhost:4100' : ''
@@ -18,8 +25,12 @@ export default class Item extends PureComponent {
     constructor() {
         super();
         this.state = {
-            imgLoad: false
+            imgLoad: false,
+            imgVisible: false
         };
+
+        this.observer;
+        this.ref;
     }
 
     handleLoad = () => this.setState({ imgLoad: true });
@@ -29,9 +40,36 @@ export default class Item extends PureComponent {
         onSelectItem(previewName, name);
     };
 
+    getRef = e => (this.ref = e);
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.observer = new IntersectionObserver(entries => {
+                const entry = entries[0];
+
+                if (entry.isIntersecting) {
+                    this.setState({
+                        imgVisible: true
+                    });
+                    this.observer.unobserve(this.ref);
+                }
+            }, defaultOptions);
+            this.observer.observe(this.ref);
+        }, LOADING_TIMEOUT);
+    }
+
+    componentWillUnmount() {
+        if (!this.observer) {
+            return;
+        }
+
+        this.observer.unobserve(this.ref);
+        this.observer.disconnect();
+    }
+
     render() {
         const { itFolder, name, selected, path, dateCreate } = this.props;
-        const { imgLoad } = this.state;
+        const { imgLoad, imgVisible } = this.state;
 
         const showNativeItem = !itFolder && !imgLoad;
         return (
@@ -42,20 +80,22 @@ export default class Item extends PureComponent {
                     'dd.MM.yyyy',
                     new Date(dateCreate)
                 )}`}
+                ref={this.getRef}
             >
                 {showNativeItem && (
                     <img className={'item-img'} src={img.file} />
                 )}
 
-                <img
-                    data-tip="hello world"
-                    src={itFolder ? img.folder : itemSrc(path)}
-                    {...classname(
-                        { 'item-img__hidden': itFolder ? false : !imgLoad },
-                        'item-img'
-                    )}
-                    onLoad={this.handleLoad}
-                />
+                {(
+                    <img
+                        src={itFolder ? img.folder : imgVisible ? itemSrc(path) : ''}
+                        {...classname(
+                            { 'item-img__hidden': itFolder ? false : !imgLoad },
+                            'item-img'
+                        )}
+                        onLoad={this.handleLoad}
+                    />
+                )}
 
                 <label
                     {...classname(
